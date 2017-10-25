cb64e9e9c69c006e174d1c5e016d720cc6a25e02
xabita <mutskxabaa@gmail.com>
2017-10-24 22:49:25 -0500

    ViewHome
    Add view homepage

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from boards import views
+
+
 urlpatterns = [
+	url(r'^homepage$', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
+
+
