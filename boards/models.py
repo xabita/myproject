6a1c0198985addf45061e107dc3f3b31cb2b931f
xabita <mutskxabaa@gmail.com>
2017-10-25 22:09:45 -0500

    DefaultPage
    Default page not found

--- boards/models.py
+++ boards/models.py
@@ -20,6 +20,7 @@ class Topic(models.Model):
 	board = models.ForeignKey(Board, related_name='topics')
 	starter = models.ForeignKey(User, related_name='topics')
 
+
 class Post(models.Model):
 	message = models.TextField(max_length=4000)
 	topic = models.ForeignKey(Topic, related_name='posts')
@@ -27,4 +28,3 @@ class Post(models.Model):
 	updated_at = models.DateTimeField(null=True)
 	created_by = models.ForeignKey(User, related_name='posts')
 	updated_by= models.ForeignKey(User, null=True, related_name='+')
-
--- boards/views.py
+++ boards/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
+from django.http import Http404
+
 from .models import Board
+
+
 
 
 # Create your views here.
@@ -9,3 +13,7 @@ from .models import Board
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
